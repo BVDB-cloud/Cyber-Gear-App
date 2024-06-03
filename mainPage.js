c7299fe6c9778608ca770c1d7a774806ff1c3ece
BVDB-cloud <123228425+BVDB-cloud@users.noreply.github.com>
2024-06-03 00:38:56 -1200

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -2,11 +2,11 @@ function renderFeaturedProducts(containerId = 'featuredProducts', productIDs = [
     const filteredProducts = products.data.filter(product => productIDs.includes(product.id));
     renderProducts(containerId, filteredProducts);
 }
-const featuredProductIDs = [1, 2, 3, 5, 9, 11];
+const featuredProductIDs = [1, 2, 3, 5, 9, 11,14];
 renderFeaturedProducts('featuredProducts', featuredProductIDs);
 function renderNewArrivals(containerId = 'newArrivalsContainer', productIDs = []) {
     const newArrivalProducts = products.data.filter(product => productIDs.includes(product.id));
     renderProducts(containerId, newArrivalProducts);
 }
-const newArrivalProductIDs = [4, 6, 7, 8, 12, 13];
+const newArrivalProductIDs = [4, 6, 7, 8, 12, 13,16];
 renderNewArrivals('newArrivalsContainer', newArrivalProductIDs);
